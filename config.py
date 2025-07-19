4c445886a98d4bebf7f2bb5db4da915d6b550fe5
Jonathan Varela <jvrela93@gmail.com>
2025-07-19 18:02:45 -0500

    Switched Database to PostgreSQL

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 class Config: 
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
-    
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
