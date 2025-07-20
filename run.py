a5d13e3b1284ccb5d9d810ca0927537c8fbada49
Jonathan Varela <jvrela93@gmail.com>
2025-07-20 11:59:24 -0500

    Edit Modal for each task, Sorting controls

--- run.py
+++ run.py
@@ -4,10 +4,10 @@ from flask import Flask
 app = create_app()
 
 # Optional: Temporary route to initialize the database on Render
-@app.route('/initdb')
-def initdb():
-    db.create_all()
-    return "Database initialized!"
+# @app.route('/initdb')
+# def initdb():
+#     db.create_all()
+#     return "Database initialized!"
 
 if __name__ == '__main__':
     app.run(debug=True)
