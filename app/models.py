d623a51c7c0bf23256d49e612f31fb7d62c4bee2
Jonathan Varela <jvrela93@gmail.com>
2025-07-20 12:00:31 -0500

    Update Task model to include due_date field

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 from flask_login import UserMixin
+from datetime import datetime
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,6 +10,8 @@ class User(UserMixin, db.Model):
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
     is_complete = db.Column(db.Boolean, default=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    due_date = db.Column(db.Date, nullable=True)  # Add this line
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
